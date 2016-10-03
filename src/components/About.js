7eec2ec7bfdb15a43288805d5b712f9cd7eacbd6
Chris Tsantiris <Chris.Tsantiris@gmail.com>
2016-10-03 11:13:27 -0400

    About screen looks pretty good. share on twitter and facebook links added

--- src/components/About.js
+++ src/components/About.js
@@ -14,8 +14,14 @@ class About extends Component {
         <p>You will play as Batman.</p>
         <p>The Riddler, an arch enemy of Batman's, has left trivia questions or riddles behind at each location.</p>
         <p>Your objective is to solve the questions in order to outsmart The Riddler. If you solve 5, you win. If you get 4 wrong, you lose.</p>
-        <a href="https://twitter.com/home?status=I'm%20playing%20Batman%20vs%20The%20Riddler.%20Click%20this%20link%20to%20check%20this%20awesome%20game%20out!%20%0Ahttp%3A//batmanvsriddler.surge.sh/%0A">Share on Twitter</a>
+        <p>
+          <a href="https://twitter.com/home?status=I'm%20playing%20Batman%20vs%20The%20Riddler.%20Click%20this%20link%20to%20check%20this%20awesome%20game%20out!%20%0Ahttp%3A//batmanvsriddler.surge.sh/%0A">Share this game on Twitter</a>
+        </p>
+        <p>
+          <a href='https://www.facebook.com/sharer/sharer.php?u=http%3A//batmanvsriddler.surge.sh/'>Share this game on Facebook</a>
+        </p>
       </div>
+      <p className='filler'>Enjoy the game!</p>
       <footer>
         <Link to='/'><button className='backbutton' onClick={this.props.resetGame}>Reset</button></Link>
       </footer>
