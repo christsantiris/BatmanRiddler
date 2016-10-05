0d0c8e642f387dfeb5a0c8d14ab6e17b5d4d1075
Chris Tsantiris <Chris.Tsantiris@gmail.com>
2016-10-05 14:48:30 -0400

    Production build for demo

--- src/components/Start.js
+++ src/components/Start.js
@@ -10,17 +10,22 @@ class Start extends Component {
 
   render () {
     return <div className='startgame'>
-      <p>The Riddler has left clues behind in different locations around Gotham City. Can you help Batman capture The Riddler by solving his riddles?</p>
-      <div className='batman'>
-          Batman
+      <div className='main'>
+        <p className='intro'>The Riddler has left clues behind in different locations around Gotham City. Can you help Batman capture The Riddler by solving his riddles?</p>
+        <div className='batman'>
+            Batman
+        </div>
       </div>
-      <select className='menu' onChange={this.props.setDifficulty}>
-        <option value='easy'>Easy</option>
-        <option value='medium'>Medium</option>
-        <option value='hard'>Hard</option>
-      </select>
-      <button className='startbutton' onClick={this.props.startGame}>Start</button>
-      <Link to='/about'><button className='aboutbutton'>About</button></Link>
+      <div className='controls'>
+        <p>Choose your difficulty and click start</p>
+        <select className='menu' onChange={this.props.setDifficulty}>
+          <option value='easy'>Easy</option>
+          <option value='medium'>Medium</option>
+          <option value='hard'>Hard</option>
+        </select>
+        <button className='startbutton' onClick={this.props.startGame}>Start</button>
+        <Link to='/about'><button className='aboutbutton'>About</button></Link>
+      </div>
     </div>
   }
 }
